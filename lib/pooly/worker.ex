f0e9c64c2a419720ab2274d03d7098376df2b41e
David Miotti <david.miotti@wopata.com>
2017-04-27 18:00:40 +0200

    Initial Elixir app

---
+++ lib/pooly/worker.ex
@@ -0,0 +1,10 @@
+
+defmodule SampleWorker do
+  use GenServer
+
+  def start_link(_), do: GenServer.start_link(__MODULE__, :ok, [])
+
+  def stop(pid), do: GenServer.call(pid, :stop)
+
+  def handle_call(:stop, _from, state), do: {:stop, :normal, :ok, state}
+end
